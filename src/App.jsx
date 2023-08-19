beab8c913aeb820332d6793eb0a6b4b5fc8edd99
Christwelve <meng.christian28@gmail.com>
2023-08-19 15:33:13 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState(""); 
+  const [task, setTask] = useState("");
+  const [editedTask, setEditedTask] = useState(""); 
+  const [editIndex, setEditIndex] = useState(-1);  
 
   const handleSubmit = (e) => {
     e.preventDefault(); 
@@ -11,25 +13,70 @@ function App() {
     setTask("");
   };
 
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setEditedTask(tasks[index]);
+  };
+
+  const handleUpdate = () => {
+    const updatedTasks = tasks.map((task, index) => {
+      if (index === editIndex) {
+        return editedTask;
+      } else {
+        return task;
+      }
+    });
+    setTasks(updatedTasks);
+    setEditIndex(-1);
+    setEditedTask("");
+  };
+  
+
+
   return (
     <div className="Home">
-      <h1>TodoReact</h1>
-
+      <a to="/">
+        <h1>TodoReact</h1>
+      </a>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter new task"
           value={task}
-          onSubmit={(e) => setTask(e.target.value)}
+          onChange={(e) => setTask(e.target.value)}
         />
         <button type="submit">Submit</button>
       </form>
 
-      <div className="list">
-        {tasks.map((taskItem, index) => (
-          <div key={index}>{taskItem}</div>
+      <ul className="list">
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editedTask}
+                  onChange={(e) => setEditedTask(e.target.value)}
+                />
+                <button onClick={handleUpdate}>Save</button>
+              </>
+            ) : (
+              <>
+                {task}
+                <button className='little_button'>Check</button>
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </>
+            )}
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
